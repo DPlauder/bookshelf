5be82eefe81c46bb3737bdeb4b9064c622de258b
Astromin21 <docgit@gmail.com>
2023-06-15 14:15:08 +0200

    js delete books button added

--- main.js
+++ main.js
@@ -1,37 +1,40 @@
 "use strict";
 
-function form_toogle(){
+function init() {
+    form_toogle();
+    change_book(shelf, 'click');
+}
+
+function form_toogle() {
     let btn = document.getElementById('add_btn');
     let form = document.getElementById('form');
     let cancel = document.getElementById('cancel');
     let send = document.getElementById('send');
     btn.addEventListener('click', () => {
         form.classList.add('form_da');
-    }) 
-    cancel.addEventListener('click',() => {
+    })
+    cancel.addEventListener('click', () => {
         console.log("cancel");
         form.classList.remove('form_da');
     })
     send.addEventListener('click', () => {
         form.classList.remove('form_da');
         newBook();
-
     })
 }
-function Book(title, autor, pages){
+function Book(title, autor, pages) {
     this.title = title,
-    this.autor = autor,
-    this.pages = pages
+        this.autor = autor,
+        this.pages = pages
     addBook();
 }
-function newBook(){
+function newBook() {
     let title = document.getElementById("title").value;
     let autor = document.getElementById("autor").value;
     let pages = document.getElementById("pages").value;
     let book = new Book(title, autor, pages);
-    
 }
-function addBook(){
+function addBook() {
     let shelf = document.getElementById('shelf');
     let entry = document.createElement('div');
     entry.classList.add('buch');
@@ -50,23 +53,70 @@ function addBook(){
 
     let entry_read = document.createElement('p')
     let check = check_read();
-    console.log(check);
-    if( check === true) {
+    if (check === true) {
         entry_read.textContent = "Read: Yes"
     } else {
         entry_read.textContent = "Read: No"
     }
     entry.appendChild(entry_read)
     shelf.appendChild(entry);
+
+    let btns = add_btns()
+    entry.appendChild(btns);
 }
-function check_read(){
+function check_read() {
     let checkbox = document.getElementById('box');
     let check = false;
-    if( checkbox.checked ){
+    if (checkbox.checked) {
         check = true;
     }
     return check;
 }
 
-form_toogle()
+function add_btns() {
+    let btnsContainer = document.createElement('div')
+    btnsContainer.classList.add('knopf');
+
+    let trash = document.createElement('div');
+    trash.classList.add('btn');
+    trash.textContent = "X"
+    btnsContainer.appendChild(trash);
+
+    let read = document.createElement('div');
+    read.classList.add('btn');
+    read.textContent = "O";
+    btnsContainer.appendChild(read);
+
+    return btnsContainer
+
+}
+
+
+
+
+
+function change_book(rootElement, event) {
+
+    rootElement.addEventListener(event, (e) => {
+        console.log(e);  
+        let targetElement = e.target;
+
+
+        while (targetElement != null) {
+            if (targetElement.textContent === 'X') {
+                targetElement.parentElement.parentElement.remove();
+            }
+
+
+            targetElement = targetElement.parentElement;
+            console.log(targetElement);
+            
+        }   
+
+
+    }, true)
+
+}
+
+init()
 
