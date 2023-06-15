e3903e104b5b27d3383d9abe8548b6874eed6177
Astromin21 <docgit@gmail.com>
2023-06-15 11:47:39 +0200

    js check Read Book added

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 "use strict";
 
-let books = [];
-
 function form_toogle(){
     let btn = document.getElementById('add_btn');
     let form = document.getElementById('form');
@@ -17,24 +15,23 @@ function form_toogle(){
     send.addEventListener('click', () => {
         form.classList.remove('form_da');
         newBook();
+
     })
 }
-
 function Book(title, autor, pages){
     this.title = title,
     this.autor = autor,
     this.pages = pages
+    addBook();
 }
-
 function newBook(){
     let title = document.getElementById("title").value;
     let autor = document.getElementById("autor").value;
     let pages = document.getElementById("pages").value;
     let book = new Book(title, autor, pages);
-    addBook(book);
+    
 }
-
-function addBook(book){
+function addBook(){
     let shelf = document.getElementById('shelf');
     let entry = document.createElement('div');
     entry.classList.add('buch');
@@ -48,12 +45,27 @@ function addBook(book){
     entry.appendChild(entry_autor);
 
     let entry_pages = document.createElement('p')
-    entry_pages.textContent = pages.value;
+    entry_pages.textContent = "Pages: " + pages.value;
     entry.appendChild(entry_pages);
-    
 
+    let entry_read = document.createElement('p')
+    let check = check_read();
+    console.log(check);
+    if( check === true) {
+        entry_read.textContent = "Read: Yes"
+    } else {
+        entry_read.textContent = "Read: No"
+    }
+    entry.appendChild(entry_read)
     shelf.appendChild(entry);
-    
+}
+function check_read(){
+    let checkbox = document.getElementById('box');
+    let check = false;
+    if( checkbox.checked ){
+        check = true;
+    }
+    return check;
 }
 
 form_toogle()
